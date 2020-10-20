0071fc8e6ef9f3e96223ec02c13ad41d3e73072d
dev-chulbuji <dev.chulbuji@gmail.com>
2020-10-21 00:40:41 +0900

    add aws iam manager

---
+++ addons/src_addon_aws_iam_manager.py
@@ -0,0 +1,50 @@
+from mitmproxy import ctx
+import boto3, json, threading, datetime
+import src_addon_logger as proxy_logger
+
+class AWSThreadManager(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self, name = "AWSThread")
+        self.period = 10
+        self.client = boto3.client('iam')
+        self.finished = threading.Event()
+        self.key_to_user = {}
+
+    def run(self):
+        while not self.finished.is_set():
+            self.get_users()
+            self.finished.wait(self.period)
+
+    def stop(self):
+        self.finished.set()
+
+    def get_users(self):
+        rtv = {}
+
+        response = self.client.list_users()
+        for user in response["Users"]:
+            username = user["UserName"]
+            access_key_metas = self.get_access_key(username)
+
+            for each in list(filter(lambda x: x["Status"] == 'Active', access_key_metas["AccessKeyMetadata"])):
+                rtv[each["AccessKeyId"]] = username
+
+        self.key_to_user = rtv
+
+        proxy_logger.info("complete to get users " + str(datetime.datetime.now()))
+
+    def get_access_key(self, userName):
+        access_keys = self.client.list_access_keys(UserName = userName)
+        return access_keys
+
+    def get_user_by_key(self, key):
+        if len(self.key_to_user.values()) == 0:
+            return ""
+
+        if not key in self.key_to_user:
+            return ""
+
+        return self.key_to_user[key]
+
+    def get_key_to_user(self):
+        return self.key_to_user
